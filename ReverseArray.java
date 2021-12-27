e82da228c7f53e7d770792c1712b5cbae1312159
unknown <seniortestengineer4@gmail.com>
2021-12-27 17:55:46 +0530

    this is my first poc

---
+++ ReverseArray.java
@@ -0,0 +1,20 @@
+
+public class ReverseArray {
+	public static void main(String args[]) 
+	{
+		int arr = new int {1,2,3,4,5,6,7};
+		System.out.println("original array");
+		for(int 1=0;i<arr.length;i++)
+		{
+		System.out.println(arr[i] +" ");
+
+		}
+		System.out.println("array in reverse order");
+        for(int i=arr.length-1;i>=0;i--) {
+        	
+    		System.out.println("arr[i] +" ");
+
+        }
+	}
+
+}
