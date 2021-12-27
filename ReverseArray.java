08104910f477fac3a3326e41be31d81dc38cc5e8
unknown <seniortestengineer4@gmail.com>
2021-12-27 18:54:54 +0530

    changes made

--- ReverseArray.java
+++ ReverseArray.java
@@ -2,7 +2,7 @@
 public class ReverseArray {
 	public static void main(String args[]) 
 	{
-		int arr = new int {1,2,3,4,5,6,7};
+		int arr = new int {1,2,3,4,5,6,7,8,9,10};
 		System.out.println("original array");
 		for(int 1=0;i<arr.length;i++)
 		{
